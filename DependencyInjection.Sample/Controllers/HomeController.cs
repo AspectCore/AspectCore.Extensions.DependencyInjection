d157bc3c31f1c65fd37addf5a6957e6a8adbc9f6
Lemon <liuhaoyang1221@hotmail.com>
2017-11-26 11:10:45 +0800

    Rebuild with microsoft.extensions.dependencyinjection 1.1

---
+++ DependencyInjection.Sample/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyInjection.Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public virtual IActionResult Index()
+        {
+            return View();
+        }
+
+        public virtual IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public virtual IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public virtual IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ DependencyInjection.Sample/Startup.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspectCore.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using AspectCore.Configuration;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace DependencyInjection.Sample
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc().AddControllersAsServices();
+
+            services.AddDynamicProxy(config =>
+            {
+                config.Interceptors.AddDelegate(async (context, next) =>
+                {
+                    Stopwatch stopwatch = Stopwatch.StartNew();
+                    await next(context);
+                    stopwatch.Stop();
+                    Console.WriteLine("Executed method {0}.{1}.{2} ({3}) in {4}ms",
+                        context.ServiceMethod.DeclaringType.Namespace,
+                        context.ServiceMethod.DeclaringType.Name,
+                        context.ServiceMethod.Name,
+                        context.ServiceMethod.DeclaringType.GetTypeInfo().Assembly.GetName().Name,
+                        stopwatch.ElapsedMilliseconds
+                    );
+                });
+            });
+
+            return services.BuildAspectCoreServiceProvider();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
